c7e14716ad26217db32b0a706f479e8ebacd17cc
kauecaires02 <84874622+kauecaires02@users.noreply.github.com>
2022-10-31 16:17:41 -0300

    the sequence
    data per extension

---
+++ relatory.py
@@ -0,0 +1,17 @@
+# dice-reverse
+# city. day, mounty year
+#The sequence / return value
+# THE BUG
+
+# dice-reverse
+# city. day, mounty year
+#The sequence / return value
+
+def data_return(c,d,m,o):
+  mount: {"1:january, 2:february, 3:march, 4:april, 5:may, 6:june, 7:july, 8:agust, 9:september, 10:october, 11:november, 12:december"}
+  de = c+", "+str(d)+" de "mount[m] "+str(a)
+return de
+
+de_ext = data_return(10,31,2022 "Brazil")
+
+print (dt_ext)
